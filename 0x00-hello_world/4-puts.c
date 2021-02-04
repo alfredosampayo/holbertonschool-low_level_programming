32c7b9bd80fca84a43bb7a6d25f778f3d97948bd
alfredosampayo <2674@holbertonschool.com>
2021-02-04 02:51:22 +0000

    Low-level programming & Algorithm 6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-int main ()
+/**
+ * main - puts function prints "Programing is like..."
+ */
+int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
