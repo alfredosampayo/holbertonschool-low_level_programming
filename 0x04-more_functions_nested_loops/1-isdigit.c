188604ca25d3bf56124267b11e58bb43654a6f69
alfredosampayo <2674@holbertonschool.com>
2021-02-11 21:52:07 +0000

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - check c if it is a digit or not
+ *
+ * Return: 1 if c is a digit otherwise return 0
+ * @c: integer for Betty
+ */
+int _isdigit(int c)
+{
+
+	if (c >= '0' && c <= '9')
+
+	return (1);
+
+	else
+
+	return (0);
+}
+
