337b015b6b86f859ae4b96a4cb55a180c9549e97
alfredosampayo <2674@holbertonschool.com>
2021-02-11 21:00:26 +0000

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _isupper - check for upper or lower case
+ *
+ * Return: 1 if c is upper case, otherwise return 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+
+	return (1);
+
+	else
+
+	return (0);
+}
