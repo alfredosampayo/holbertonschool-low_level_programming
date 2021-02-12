77049d790c8310b16073d737a0d8c71a4ead4f6f
alfredosampayo <2674@holbertonschool.com>
2021-02-12 16:03:58 +0000

    Collaboration is multiplication2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,16 +2,10 @@
 /**
  * mul - multiplies 2 integers
  * Return: resultant of two multiplied integers
- * @c:1
+ * @a: multiplier
+ * @b: multiplier
  */
 int mul(int a, int b)
 {
-	if (b == 0)
-		return (0);
-	if (b > 0)
-
-		return (a + mul(a, b-1));
-
-	if (b < 0)
-		return -mul(a, -b);
+		return (a * b);
 }
