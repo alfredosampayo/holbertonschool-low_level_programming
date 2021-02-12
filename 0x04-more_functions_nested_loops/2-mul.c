f5c55c3a9477ed042fc2db86730147add3028360
alfredosampayo <2674@holbertonschool.com>
2021-02-12 05:06:51 +0000

    multiplies two integers2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ * mul - multiplies 2 integers
+ * Return: resultant of two multiplied integers
+ * @c:1
  */
 int mul(int a, int b)
 {
-	if((a == 0) || (b == 0))
-		return 0;
+	if ((a == 0) || (b == 0))
+		return (0);
 	else
 
-	return (a + mul(a, b - 1)); 
+	return (a + mul(a, b - 1));
 }
