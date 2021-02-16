1d8255d03c9cacf86086e4532bd8331e2dfd7a5a
alfredosampayo <2674@holbertonschool.com>
2021-02-16 19:19:35 +0000

    Updated - corrected commands/code

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *
+ *
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+	
+	temp = *a;
+	*a = *b;
+	*b = temp;
+	
+}
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,16 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+
+int _strlen(char *s);
+void swap_int(int *a, int *b);
+void reset_to_98(int *n);
+int _isupper(int c);
+void positive_or_negative(int i);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+int _putchar(char c);
+void print_most_numbers(void);
+void more_numbers(void);
+#endif /* HOLBERTON_H */
