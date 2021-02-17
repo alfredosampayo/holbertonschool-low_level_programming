03f535ba3b83afe169f4cf98d55fe182599892ce
alfredo.sampayo@me.com <2674@holbertonschool.com>
2021-02-17 20:39:22 +0000

    prints string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,2 +1,19 @@
 #include "holberton.h"
+/**
+ * _puts - prints a string
+ * @str: prints a string
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; (*(str + i)) != '\0'; i++)
+
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
 
