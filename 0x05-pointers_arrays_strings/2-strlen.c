d9f6be860102ca87f9095788af5d7fa97edc4545
alfredo.sampayo@me.com <2674@holbertonschool.com>
2021-02-17 17:38:48 +0000

    Corrected code

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,19 +1,19 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ * _strlen - return lenght of string
+ * @s: variable
+ * Return: c
  */
 int _strlen(char *s)
 {
 
 	int i;
 	int c = 0;
-	
-	for (i=0; (*(s + i)) != '\0' ; i++)
-	}
+
+	for (i = 0; (*(s + i)) != '\0';  i++)
+	{
 		c++;
-	{	
-	
+	}
+
 	return (c);
 }
