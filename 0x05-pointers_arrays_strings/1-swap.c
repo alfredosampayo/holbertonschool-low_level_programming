8e050e39b7cc022e89e622f35ab17480879ed183
alfredo.sampayo@me.com <2674@holbertonschool.com>
2021-02-17 17:16:08 +0000

    corrected comments

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,16 @@
 #include "holberton.h"
 /**
- *
- *
+ * swap_int - swap values of integers
+ * @a: swapping variable
+ * @b: swapping variable
+ * Return: void
  */
 void swap_int(int *a, int *b)
 {
 	int temp;
-	
+
 	temp = *a;
 	*a = *b;
 	*b = temp;
-	
+
 }
