b885770d402daed7c9f4b2c423cef2d35e2821a1
alfredosampayo <2674@holbertonschool.com>
2021-02-17 15:54:54 +0000

    corrected code

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,4 +5,15 @@
  *
  */
 int _strlen(char *s)
+{
 
+	int i;
+	int c = 0;
+	
+	for (i=0; (*(s + i)) != '\0' ; i++)
+	}
+		c++;
+	{	
+	
+	return (c);
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -3,6 +3,8 @@
 #include <stdio.h>
 
 int _strlen(char *s);
+void _puts(char *str);
+int _strlen(char *s);
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
 int _isupper(int c);
