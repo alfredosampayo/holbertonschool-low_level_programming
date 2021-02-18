7246f68cf9279ec382da65dc886659d31b6da488
alfredo.sampayo@me.com <2674@holbertonschool.com>
2021-02-18 04:34:58 +0000

    half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * puts_half - prints second half
+ * Return: void
+ * @str: variable
+ */
+void puts_half(char *str)
+{
+	int i;
+	
+	for ( i = 0; str[i] != '\0'; i++)
+	{
+		;
+	}
+		_putchar('\n');
+}
+
+
