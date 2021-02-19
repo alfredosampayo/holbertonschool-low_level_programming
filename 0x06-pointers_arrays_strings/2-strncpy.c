52a1ffdc5dfeec544ba423f6885e5c07c37b0d09
alfredo.sampayo@me.com <2674@holbertonschool.com>
2021-02-19 03:25:28 +0000

    updated code

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,27 +1,29 @@
 #include "holberton.h"
 /**
- * *_strncat - concagnate 2 strings
- * Return: 0
+ * *_strncpy - copy  strings
+ * Return: dest
  * @dest: variable
  * @src: variable
  * @n:  variable
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-        int i;
+	int i;
 
-        int x;
+	int x;
 
-        for (i = 0; dest[i] != '\0'; i++)
-        ;
-        for (x = 0; src[x] != '\0' && n > 0; x++, i++)
+	for (i = 0; dest[i] != '\0'; i++)
 
-        {
+	;
 
-                dest[i] = src[x];
-                i++;
-        }
+for (x = 0; src[x] != '\0' && n > 0; x++, i++)
+
+	{
+
+		 dest[i] = src[x];
+		i++;
+	}
 
 
-        return (dest);
+	return (dest);
 }
