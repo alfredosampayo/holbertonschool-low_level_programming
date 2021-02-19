e5f01964b3f810fbacf094c6ed1bf9885293813f
alfredo.sampayo@me.com <2674@holbertonschool.com>
2021-02-19 05:35:03 +0000

    More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,29 @@
+#included "holberton.h"
+/**
+ * _array - reverses content of array
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+ void reverse_array(int *a, int n)
+{
+	int i;
+
+	int x;
+
+	for (i = 0; a[i] != '\0'; i++)
+	;
+	for (x = 0; n[x] != '\0'; x++, i++);
+
+	{
+		a[i] = n[x]
+
+	}
+
+		a[i + 1] = '\0';
+
+	return (0);
+
+}
+
