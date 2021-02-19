9061ef3eed7cb59bf03248651120fc09c9c152bd
alfredo.sampayo@me.com <2674@holbertonschool.com>
2021-02-19 00:46:23 +0000

    More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,25 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+
+char *_strcat(char *dest, char *src);
+int _atoi(char *s);
+char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
+void puts_half(char *str);
+void puts2(char *str);
+void rev_string(char *s);
+void print_rev(char *s);
+int _strlen(char *s);
+void _puts(char *str);
+int _strlen(char *s);
+void swap_int(int *a, int *b);
+void reset_to_98(int *n);
+int _isupper(int c);
+void positive_or_negative(int i);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+int _putchar(char c);
+void print_most_numbers(void);
+#endif /* HOLBERTON_H */
